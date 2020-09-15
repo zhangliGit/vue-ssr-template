a07941e33a236306c559afc37048768c13b3461f
e <18702707106@163.com>
2020-09-15 17:46:45 +0800

    初始化模板

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="about">我是关于界面</div>
+    <div @click="back">返回啦</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      title: '我是关于界面',
+    }
+  },
+  methods: {
+    back() {
+      this.$router.back()
+    },
+  },
+}
+</script>
+<style scoped>
+.about {
+  color: red;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">vue-ssr-app</h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+        <div @click="goAbout">我要跳转</div>
+        <div @click="goNews">我是新闻界面</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goAbout() {
+      this.$router.push('/about')
+    },
+    goNews() {
+      this.$router.push('/news/newList')
+    },
+  },
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
