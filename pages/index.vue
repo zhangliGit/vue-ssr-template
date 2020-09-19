1dbf03850b2e4bffbd523c9c5c3c624e896828ba
zhangli <zhangli>
2020-09-19 22:37:31 +0800

    代码优化

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     </div>
     <el-row>
       <el-col :xs="12" :sm="12" :lg="8">{{ title }}</el-col>
-      <el-col :xs="12" :sm="12" :lg="8">2</el-col>
+      <el-col :xs="12" :sm="12" :lg="8">{{ tag }}</el-col>
       <el-col :xs="12" :sm="12" :lg="8">3</el-col>
     </el-row>
   </div>
@@ -15,13 +15,26 @@
 import { mapState, mapActions } from 'vuex'
 export default {
   components: {},
-  asyncData() {},
+  async asyncData({ $axios }) {
+    await $axios.get(
+      `http://canpointtest.com:8090/videoApi/getVideo?page=1&size=20`
+    )
+    return {
+      tag: 2,
+    }
+  },
+  data() {
+    return {
+      tag: '1',
+    }
+  },
   computed: {
     ...mapState('home', ['title']),
   },
   async mounted() {
-    const res = await this.getData()
-    console.log(res)
+    // const res = await this.getData()
+    // this.tag = 2
+    // console.log(res)
   },
   methods: {
     ...mapActions('home', ['getData']),
